a084ef2ac23734237d1184b54b8326673823e98c
Sebastien G <sebastieng@gmail.com>
2026-03-03 23:20:16 -0500

    Amélioration de l'aspect de la page de vérification Turnstile

--- application/views/challenge.php
+++ application/views/challenge.php
@@ -2,28 +2,53 @@
 <head>
 	<title>CLG > Chimie > Vérification</title>
 
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400&subset=latin,latin-ext" type="text/css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600&family=Zilla+Slab:wght@300;400;600&display=swap">
+
 	<script src="https://challenges.cloudflare.com/turnstile/v0/api.js" async defer></script>
 </head>
 
 <body style="font-family: Arial">
 
-	<div style="margin: auto; text-align: center">
+	<div style="margin: auto; margin-top: 50px; text-align: center">
 
-		<p style="margin-top: 45px; font-weight: bold; font-size: 1.2em">Collège Lionel-Groulx > Département de chimie</p>
+		<img id="footer-logo" src="<?= base_url() . 'assets/img/logoCLG_2019.svg'; ?>" />
 
-		<p style="margin-top: 40px;">
+		<p style="margin-top: 30px">
+			<span style="vertical-align: middle; font-family: Montserrat; font-weight: 600; font-size: 2.4em;">
+				<img class="d-inline" src="<?= base_url() . 'assets/img/logoCLG_i.png'; ?>" style="vertical-align: top; height: 30px; margin-top: 6px;"  />
+				CHIMIE
+			</span>
+		</p>
+
+		<p style="font-family: Lato; font-weight: 300; margin-top: 30px; margin-bottom: 20px;">
 			Nous sommes désolés de cette interruption.
 		</p>
 
 	</div>
 
-		<div style="margin-auto: margin-top: 40px; text-align: center" 
-			 class="cf-turnstile"
-			 data-sitekey="<?= $this->config->item('cf_turnstile')['site_key']; ?>"
-			 data-callback="onTurnstileSuccess"
-			 data-action="bot-redirect"
-			 data-theme="auto">
-		</div>
+		<? if ($this->config->item('is_DEV')) : ?>
+
+			<div style="margin-auto: margin-top: 50px; text-align: center" 
+				 class="cf-turnstile" 
+				 data-sitekey="1x00000000000000000000AA"
+				 data-theme="light">
+			</div>
+
+		<? else : ?>
+
+			<div style="margin-auto: margin-top: 50px; text-align: center" 
+				 class="cf-turnstile"
+				 data-sitekey="<?= $this->config->item('cf_turnstile')['site_key']; ?>"
+				 data-callback="onTurnstileSuccess"
+				 data-action="bot-redirect"
+				 data-theme="auto">
+			</div>
+
+		<? endif; ?>
 
 		<form id="cf-form" method="post" action="/bot/verify_turnstile">
 			<input type="hidden" name="cf-turnstile-response" id="cf-response">
